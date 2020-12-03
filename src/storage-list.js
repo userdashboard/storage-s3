623b540d70264e0d107f0ea58638cc208f2d9ae1
benlowry <benlowry@outlook.com>
2020-12-03 08:32:42 -0800

    Tweaking test delay

--- src/storage-list.js
+++ src/storage-list.js
@@ -206,7 +206,7 @@ module.exports = {
             Log.error('error writing', error)
             return callback(new Error('unknown-error'))
           }
-          return setTimeout(callback, 10)
+          return setTimeout(callback, 0)
         })
       })
     }
