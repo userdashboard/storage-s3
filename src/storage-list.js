fd3d64e4394c16da9316a5a7ba60f05636650f43
benlowry <benlowry@outlook.com>
2020-12-03 07:32:20 -0800

    Reduce timeout on testing

--- src/storage-list.js
+++ src/storage-list.js
@@ -206,7 +206,7 @@ module.exports = {
             Log.error('error writing', error)
             return callback(new Error('unknown-error'))
           }
-          return setTimeout(callback, 100)
+          return setTimeout(callback, 10)
         })
       })
     }
